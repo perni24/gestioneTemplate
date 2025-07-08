5c0d5c1db896395fbec14262ecbb868e3dbeecc1
perni24 <perni24lb@gmail.com>
2025-07-08 17:17:16 +0200

    miglioramento grafico anteprima

--- src/components/anteprima.jsx
+++ src/components/anteprima.jsx
@@ -1,8 +1,11 @@
 function Anteprima(html){
     return(
         <>
-            <div className="flex flex-col w-full h-full overflow-hidden">
-                <div className="flex justify-between">
+            <div className="flex flex-col justify-items-center w-full h-full overflow-hidden">
+                <div className="w-5/6 h-full bg-gray-200 overflow-y-auto p-4">
+
+                </div>
+                <div className="flex w-full justify-around mb-4">
                     <button className="btn w-1/6">Editor</button>
                     <button className="btn w-1/6">Scarica</button>
                     <button className="btn w-1/6">Elimina</button>
