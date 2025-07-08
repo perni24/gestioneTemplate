3f1f756f04134ae6a1b68fb28eba34ee8efbfdeb
perni24 <perni24lb@gmail.com>
2025-07-08 17:58:44 +0200

    stile anteprima v2

--- src/components/anteprima.jsx
+++ src/components/anteprima.jsx
@@ -1,12 +1,12 @@
 function Anteprima(html){
     return(
         <>
-            <div className="flex flex-col justify-items-center w-full h-full overflow-hidden">
-                <div className="w-5/6 h-full bg-gray-200 overflow-y-auto p-4">
+            <div className="flex flex-col items-center w-full h-full overflow-hidden">
+                <div className="w-full h-full bg-gray-200 overflow-y-auto p-4">
 
                 </div>
-                <div className="flex w-full justify-around mb-4">
-                    <button className="btn w-1/6">Editor</button>
+                <div className="flex flex-row-reverse w-full justify-around m-4">
+                    <button className="btn w-1/6">Modifica</button>
                     <button className="btn w-1/6">Scarica</button>
                     <button className="btn w-1/6">Elimina</button>
                 </div>
