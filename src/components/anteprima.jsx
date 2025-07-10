356f962647ce9bda8eff7779fac58d762aacb8b6
perni24 <perni24lb@gmail.com>
2025-07-10 17:57:46 +0200

    modale elimina tempalte 50%

--- src/components/anteprima.jsx
+++ src/components/anteprima.jsx
@@ -1,3 +1,4 @@
+
 function Anteprima(html) {
     return (
         <>
@@ -16,13 +17,16 @@ function Anteprima(html) {
                     <button className="btn w-1/6">Elimina</button>
                 </div>
             </div>
-            <div className="flex flex-col items-center w-full border border-gray-400 rounded-lg p-4">
-                <p className="">Sei sicuro di voler eliminare il template?</p>
-                <div className="flex flex-row justify-around w-full">
-                    <button className="btn w-1/6">Si</button>
-                    <button className="btn w-1/6" >No</button>
+            <div className=" absolute inset-0 flex items-center justify-center bg-black/50">
+                <div className="flex flex-col items-center bg-gray-700 border border-gray-400 rounded-lg p-4 w-2/6 ">
+                    <p className="p-10 text-white">Sei sicuro di voler eliminare il template?</p>
+                    <div className="flex flex-row justify-around w-full">
+                        <button className="btn w-1/6">Si</button>
+                        <button className="btn w-1/6" >No</button>
+                    </div>
                 </div>
             </div>
+
         </>
     )
 }
